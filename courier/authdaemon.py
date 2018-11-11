9007fee42952d96b18793753ccdd53fd3e1fce69
Gordon Messmer <gordon@dragonsdawn.net>
2018-11-11 09:35:58 -0800

    Python 3 and PEP 8 updates. 2to3 and pylint used to identify and fix code.

--- courier/authdaemon.py
+++ courier/authdaemon.py
@@ -22,25 +22,25 @@ import socket
 import courier.config
 
 
-socketPath = '/var/spool/authdaemon/socket'
-_timeoutSock = 10
-_timeoutWrite = 10
-_timeoutRead = 30
+socket_path = '/var/spool/authdaemon/socket'
+TIMEOUT_SOCK = 10
+TIMEOUT_WRITE = 10
+TIMEOUT_READ = 30
 
 
 def _setup():
-    courier.config.applyModuleConfig('authdaemon.py', globals())
+    courier.config.apply_module_config('authdaemon.py', globals())
 
 
 def _connect():
     try:
-        authSock = socket.socket(socket.AF_UNIX)
+        auth_sock = socket.socket(socket.AF_UNIX)
     except socket.error:
         raise IOError('could not create socket')
-    if _timeoutSock == 0:
+    if TIMEOUT_SOCK == 0:
         try:
-            authSock.connect(socketPath)
-            authSock.setblocking(0)
+            auth_sock.connect(socket_path)
+            auth_sock.setblocking(0)
         except socket.error:
             raise IOError('could not connect to authdaemon socket')
     else:
@@ -48,24 +48,24 @@ def _connect():
         # to throw an error, indicating that the connection is in progress.
         # Use select to wait for the connection to complete, and then check
         # for errors with getsockopt.
-        authSock.setblocking(0)
+        auth_sock.setblocking(0)
         try:
-            authSock.connect(socketPath)
-        except socket.error, e:
-            if e[0] != errno.EINPROGRESS:
-                raise IOError('connection failed, error: %d, "%s"' % (e[0], e[1]))
-            readySocks = select.select([authSock], [], [], _timeoutSock)
-            if authSock in readySocks[0]:
-                soError = authSock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
-                if soError:
-                    raise IOError('connection failed, error: %d' % soError)
+            auth_sock.connect(socket_path)
+        except socket.error as e:
+            if e.errno != errno.EINPROGRESS:
+                raise IOError('connection failed, error: %d, "%s"' % (e.errno, e.strerror))
+            ready_socks = select.select([auth_sock], [], [], TIMEOUT_SOCK)
+            if auth_sock in ready_socks[0]:
+                so_error = auth_sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
+                if so_error:
+                    raise IOError('connection failed, error: %d' % so_error)
             else:
                 # The connection timed out.
                 raise IOError('connection timed out')
-    return authSock
+    return auth_sock
 
 
-def _writeAuth(authSock, cmd):
+def _write_auth(auth_sock, cmd):
     try:
         # Loop: Wait for select() to indicate that the socket is ready
         # for data, and call send().  If send returns a value smaller
@@ -73,10 +73,10 @@ def _writeAuth(authSock, cmd):
         # continue to attempt to send it.  If select() times out, raise
         # an exception and let the handler close the connection.
         while cmd:
-            readySocks = select.select([], [authSock], [], _timeoutWrite)
-            if not readySocks[1]:
+            ready_socks = select.select([], [auth_sock], [], TIMEOUT_WRITE)
+            if not ready_socks[1]:
                 raise socket.error('Write timed out.')
-            sent = authSock.send(cmd)
+            sent = auth_sock.send(cmd)
             if sent < len(cmd):
                 cmd = cmd[sent:]
             else:
@@ -86,15 +86,15 @@ def _writeAuth(authSock, cmd):
         raise IOError('connection to authdaemon lost while sending request')
 
 
-def _readAuth(authSock, term):
+def _read_auth(auth_sock, term):
     data = ''
     datal = 0
     terml = len(term)
     while 1:
-        readySocks = select.select([authSock], [], [], _timeoutRead)
-        if not readySocks[0]:
+        ready_socks = select.select([auth_sock], [], [], TIMEOUT_READ)
+        if not ready_socks[0]:
             raise IOError('timeout when reading authdaemon reply')
-        buf = authSock.recv(1024)
+        buf = auth_sock.recv(1024)
         if not buf:
             raise IOError('connection closed when reading authdaemon reply')
         data += buf
@@ -107,28 +107,31 @@ def _readAuth(authSock, term):
     return data.split('\n')
 
 
-def _doAuth(cmd):
+def _do_auth(cmd):
     """Send cmd to the authdaemon, and return a dictionary containing its reply."""
-    authSock = _connect()
-    _writeAuth(authSock, cmd)
-    authData = _readAuth(authSock, '\n.\n')
-    authInfo = {}
-    for authLine in authData:
-        if authLine == 'FAIL':
+    auth_sock = _connect()
+    _write_auth(auth_sock, cmd)
+    auth_data = _read_auth(auth_sock, '\n.\n')
+    auth_info = {}
+    for auth_line in auth_data:
+        if auth_line == 'FAIL':
             # Return None, discarding all other data from authdaemon
             return None
-        if '=' not in authLine:
+        if '=' not in auth_line:
             continue
-        (authKey, authVal) = authLine.split('=',1)
-        authInfo[authKey] = authVal
-    return authInfo
+        (auth_key, auth_val) = auth_line.split('=', 1)
+        auth_info[auth_key] = auth_val
+    return auth_info
 
 
-def getUserInfo(service, uid):
+def get_user_info(service, uid):
     cmd = 'PRE . %s %s\n' % (service, uid)
-    userInfo = _doAuth(cmd)
-    return userInfo
+    user_info = _do_auth(cmd)
+    return user_info
 
 
 # Call _setup to correct the socket path
 _setup()
+
+# Deprecated names preserved for compatibility with older releases
+getUserInfo = get_user_info
