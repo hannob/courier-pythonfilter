809f09462103630fec4e6aa017ee526d044f2e06
Gordon Messmer <gordon@dragonsdawn.net>
2019-01-12 00:04:05 -0800

    Python 3 and PEP 8 updates. 2to3 and pylint used to identify and fix code.

--- filters/pythonfilter/auto_whitelist.py
+++ filters/pythonfilter/auto_whitelist.py
@@ -22,42 +22,42 @@ import sys
 import time
 import courier.config
 import courier.control
-import TtlDb
+from . import TtlDb
 
 
 # The good/bad senders lists will be scrubbed at the interval indicated
 # in seconds.  All records older than the "whitelistTTL" number of seconds
 # will be removed from the lists.
-whitelistTTL = 60 * 60 * 24 * 30
-whitelistPurgeInterval = 60 * 60 * 12
+whitelist_ttl = 60 * 60 * 24 * 30
+whitelist_purge_interval = 60 * 60 * 12
 
 
-def initFilter():
-    courier.config.applyModuleConfig('auto_whitelist.py', globals())
+def init_filter():
+    courier.config.apply_module_config('auto_whitelist.py', globals())
     # Keep a dictionary of sender/recipient pairs that we've seen before
     try:
         global _whitelist
-        _whitelist = TtlDb.TtlDb('auto_whitelist', whitelistTTL, whitelistPurgeInterval)
-    except TtlDb.OpenError, e:
+        _whitelist = TtlDb.TtlDb('auto_whitelist', whitelist_ttl, whitelist_purge_interval)
+    except TtlDb.OpenError as e:
         sys.stderr.write('Could not open auto_whitelist TtlDb: %s\n' % e)
         sys.exit(1)
     # Record in the system log that this filter was initialized.
     sys.stderr.write('Initialized the "auto_whitelist" python filter\n')
 
 
-def _whitelistRecipients(controlFileList):
-    sender = courier.control.getSender(controlFileList).lower()
-    senderMd5 = hashlib.md5(sender)
+def whitelist_recipients(control_files):
+    sender = courier.control.get_sender(control_files).lower()
+    sender_md5 = hashlib.md5(sender)
     _whitelist.lock()
     try:
-        for recipient in courier.control.getRecipients(controlFileList):
+        for recipient in courier.control.get_recipients(control_files):
             recipient = recipient.lower()
             # Don't allow a whitelist between identical addresses.  Users
             # sometimes email themselves a note, which creates a path for
             # spam.
             if recipient == sender:
                 continue
-            correspondents = senderMd5.copy()
+            correspondents = sender_md5.copy()
             correspondents.update(recipient)
             cdigest = correspondents.hexdigest()
             _whitelist[cdigest] = time.time()
@@ -65,24 +65,24 @@ def _whitelistRecipients(controlFileList):
         _whitelist.unlock()
 
 
-def _checkWhitelist(controlFileList):
-    foundAll = 1
-    sender = courier.control.getSender(controlFileList).lower()
+def check_whitelist(control_files):
+    found_all = 1
+    sender = courier.control.get_sender(control_files).lower()
     _whitelist.lock()
     try:
-        for recipient in courier.control.getRecipients(controlFileList):
+        for recipient in courier.control.get_recipients(control_files):
             correspondents = hashlib.md5(recipient.lower())
             correspondents.update(sender)
             cdigest = correspondents.hexdigest()
             if not cdigest in _whitelist:
-                foundAll = 0
+                found_all = 0
                 break
     finally:
         _whitelist.unlock()
-    return foundAll
+    return found_all
 
 
-def doFilter(bodyFile, controlFileList):
+def do_filter(body_file, control_files):
     """Return a 200 code if the message looks like a reply to a message
     sent by an authenticated user.
 
@@ -93,16 +93,14 @@ def doFilter(bodyFile, controlFileList):
     """
 
     _whitelist.purge()
-    authUser = courier.control.getAuthUser(controlFileList, bodyFile)
-    if authUser:
-        _whitelistRecipients(controlFileList)
+    auth_user = courier.control.get_auth_user(control_files, body_file)
+    if auth_user:
+        whitelist_recipients(control_files)
         return ''
-    else:
-        if _checkWhitelist(controlFileList):
-            return '200 Ok'
-        else:
-            # Return no decision for everyone else.
-            return ''
+    if check_whitelist(control_files):
+        return '200 Ok'
+    # Return no decision for everyone else.
+    return ''
 
 
 if __name__ == '__main__':
@@ -111,8 +109,8 @@ if __name__ == '__main__':
     # and more lines, beginning with an 'r' character, for each
     # recipient.  Run this script with the name of that file as an
     # argument, and it'll validate that email address.
-    if not len(sys.argv) == 3:
-        print 'Use:  auto_whitelist.py <body file> <control file>'
+    if len(sys.argv) != 3:
+        print('Use:  auto_whitelist.py <body file> <control file>')
         sys.exit(1)
-    initFilter()
-    print doFilter(sys.argv[1], sys.argv[2:])
+    init_filter()
+    print(do_filter(sys.argv[1], sys.argv[2:]))
