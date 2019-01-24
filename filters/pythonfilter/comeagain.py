c09ff119e76bc5849a7ab6463075a689849c46f0
Gordon Messmer <gordon@dragonsdawn.net>
2019-01-23 21:39:12 -0800

    Python 3 and PEP 8 updates. 2to3 and pylint used to identify and fix code.

--- filters/pythonfilter/comeagain.py
+++ filters/pythonfilter/comeagain.py
@@ -22,30 +22,30 @@ import sys
 import time
 import courier.config
 import courier.control
-import TtlDb
+from . import TtlDb
 
 
 # The good/bad senders lists will be scrubbed at the interval indicated
-# in seconds.  All records older than the "sendersTTL" number of seconds
+# in seconds.  All records older than the "senders_ttl" number of seconds
 # will be removed from the lists.
-sendersTTL = 60 * 60 * 24 * 30
-sendersPurgeInterval = 60 * 60 * 12
+senders_ttl = 60 * 60 * 24 * 30
+senders_purge_interval = 60 * 60 * 12
 
 
-def initFilter():
-    courier.config.applyModuleConfig('comeagain.py', globals())
+def init_filter():
+    courier.config.apply_module_config('comeagain.py', globals())
     # Keep a dictionary of sender/recipient pairs that we've seen before
     try:
         global _senders
-        _senders = TtlDb.TtlDb('correspondents', sendersTTL, sendersPurgeInterval)
-    except TtlDb.OpenError, e:
+        _senders = TtlDb.TtlDb('correspondents', senders_ttl, senders_purge_interval)
+    except TtlDb.OpenError as e:
         sys.stderr.write('Could not open comeagain TtlDb: %s\n' % e)
         sys.exit(1)
     # Record in the system log that this filter was initialized.
     sys.stderr.write('Initialized the "comeagain" python filter\n')
 
 
-def doFilter(bodyFile, controlFileList):
+def do_filter(body_file, control_files):
     """Return a temporary failure message if this sender hasn't tried to
     deliver mail previously.
 
@@ -60,8 +60,8 @@ def doFilter(bodyFile, controlFileList):
 
     # Grab the sender from the control files.
     try:
-        sender = courier.control.getSender(controlFileList)
-    except:
+        sender = courier.control.get_sender(control_files)
+    except Exception:
         return '451 Internal failure locating control files'
     if sender == '':
         # Null sender is allowed as a non-fatal error
@@ -71,29 +71,28 @@ def doFilter(bodyFile, controlFileList):
 
     # Create a new MD5 object.  The pairs of sender/recipient will
     # be stored in the db in the form of an MD5 digest.
-    senderMd5 = hashlib.md5(sender)
+    sender_md5 = hashlib.md5(sender)
 
     # Create a digest for each recipient and look it up in the db.
     # Update the timestamp of each pair as we look them up.  If any
     # pair does not exist, we'll have to ask the sender to deliver
     # again.
-    foundAll=1
+    found_all = 1
     _senders.lock()
     try:
-        for recipient in courier.control.getRecipients(controlFileList):
-            correspondents = senderMd5.copy()
+        for recipient in courier.control.get_recipients(control_files):
+            correspondents = sender_md5.copy()
             correspondents.update(recipient)
             cdigest = correspondents.hexdigest()
             if not cdigest in _senders:
-                foundAll = 0
+                found_all = 0
             _senders[cdigest] = time.time()
     finally:
         _senders.unlock()
 
-    if foundAll:
+    if found_all:
         return ''
-    else:
-        return('421 Please send the message again to prove you are not spamware or virusware.')
+    return '421 Please send the message again to prove you are not spamware or virusware.'
 
 
 if __name__ == '__main__':
@@ -103,7 +102,7 @@ if __name__ == '__main__':
     # recipient.  Run this script with the name of that file as an
     # argument, and it'll validate that email address.
     if not sys.argv[1:]:
-        print 'Use: comeagain.py <control file>'
+        print('Use: comeagain.py <control file>')
         sys.exit(1)
-    initFilter()
-    print doFilter('', sys.argv[1:])
+    init_filter()
+    print(do_filter('', sys.argv[1:]))
