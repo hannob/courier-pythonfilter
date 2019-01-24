64b63753f92b1e6ca46e39c38697ceec8a390759
Gordon Messmer <gordon@dragonsdawn.net>
2019-01-23 22:11:27 -0800

    Discontinue use of the ThreadSMTP class, in the vague hope that whatever bug caused deadlocks in the past is gone now.

--- filters/pythonfilter/dialback.py
+++ filters/pythonfilter/dialback.py
@@ -17,9 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pythonfilter.  If not, see <http://www.gnu.org/licenses/>.
 
-import errno
 import hashlib
-import select
 import smtplib
 import socket
 import sys
