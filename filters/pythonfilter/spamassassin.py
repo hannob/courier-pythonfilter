1017202e0ac14bd22a1ee796c2c06ccd08d88d9a
Gordon Messmer <gordon@dragonsdawn.net>
2019-02-03 14:34:42 -0800

    Python 3 and PEP 8 updates. 2to3 and pylint used to identify and fix code.

--- filters/pythonfilter/spamassassin.py
+++ filters/pythonfilter/spamassassin.py
@@ -17,91 +17,91 @@
 # You should have received a copy of the GNU General Public License
 # along with pythonfilter.  If not, see <http://www.gnu.org/licenses/>.
 
-import os.path
-import sys
-import commands
 import email
+import os.path
+import subprocess
+import sys
 import courier.config
 import courier.xfilter
 
 
-spamcPath = '/usr/bin/spamc'
+spamc_path = '/usr/bin/spamc'
 # This is the maximum size of a message that we'll try to scan.
 # 500 KiB is spamc's default.
-maxMsgSize = 512000
+max_msg_size = 512000
 # If you want to scan messages as a user other than the one as
 # which pythonfilter runs, specify the user's name in the modules
 # configuration file.
 username = None
-# If rejectScore is set to a number, then the score in the X-Spam-Status
+# If reject_score is set to a number, then the score in the X-Spam-Status
 # header will be used to determine whether or not to reject the message.
 # Otherwise, messages will be rejected if they are spam.
-rejectScore = None
+reject_score = None
 
 
-def initFilter():
-    courier.config.applyModuleConfig('spamassassin.py', globals())
+def init_filter():
+    courier.config.apply_module_config('spamassassin.py', globals())
     # Record in the system log that this filter was initialized.
     sys.stderr.write('Initialized the "spamassasinfilter" python filter\n')
 
 
-def checkRejectCondition(status, resultHeader):
-    if not resultHeader:
-        resultHeader = ''
+def check_reject_condition(status, result_header):
+    if not result_header:
+        result_header = ''
     else:
-        resultHeader = resultHeader.replace('\n', '')
-    if rejectScore is None or resultHeader == '':
-        # No rejectScore is configured or spamassassin is configured not
+        result_header = result_header.replace('\n', '')
+    if reject_score is None or result_header == '':
+        # No reject_score is configured or spamassassin is configured not
         # to create new headers, so simply use the exit status of
         # spamc.  If the exit status is not 0, then the message is spam.
         if status != 0:
-            return '554 Mail rejected - spam detected: ' + resultHeader
-    elif resultHeader.startswith('Yes,'):
-        # Attempt to load the score from the resultHeader.
-        resultwords = resultHeader.split()
+            return '554 Mail rejected - spam detected: ' + result_header
+    elif result_header.startswith('Yes,'):
+        # Attempt to load the score from the result_header.
+        resultwords = result_header.split()
         for word in resultwords:
             if word.startswith('score='):
                 score = float(word[6:])
-                if score >= rejectScore:
-                    return '554 Mail rejected - spam detected: ' + resultHeader
+                if score >= reject_score:
+                    return '554 Mail rejected - spam detected: ' + result_header
     return None
 
 
-def doFilter(bodyFile, controlFileList):
-    msgSize = os.path.getsize(bodyFile)
-    if msgSize > maxMsgSize:
+def do_filter(body_file, control_files):
+    msg_size = os.path.getsize(body_file)
+    if msg_size > max_msg_size:
         return ''
     try:
         userarg = ''
         if username:
             userarg = ' -u ' + username
-        cmd = '%s %s -s %d -E < %s' % (spamcPath, userarg, maxMsgSize, bodyFile)
-        (status,output) = commands.getstatusoutput(cmd)
-    except Exception, e:
+        cmd = '%s %s -s %d -E < %s' % (spamc_path, userarg, max_msg_size, body_file)
+        (status, output) = subprocess.getstatusoutput(cmd)
+    except Exception as e:
         return "454 " + str(e)
 
     # Parse the output of spamc into an email.message object.
     result = email.message_from_string(output)
-    resultHeader = result['X-Spam-Status']
+    result_header = result['X-Spam-Status']
 
-    rejectMsg = checkRejectCondition(status, resultHeader)
-    if rejectMsg is not None:
-        return rejectMsg
+    reject_msg = check_reject_condition(status, result_header)
+    if reject_msg is not None:
+        return reject_msg
 
     # If the message wasn't rejected, then replace the message with
     # the output of spamc.
-    mfilter = courier.xfilter.XFilter('spamassassin', bodyFile,
-                                      controlFileList)
+    mfilter = courier.xfilter.XFilter('spamassassin', body_file,
+                                      control_files)
     mfilter.setMessage(result)
-    submitVal = mfilter.submit()
-    return submitVal
+    submit_val = mfilter.submit()
+    return submit_val
 
 
 if __name__ == '__main__':
     # we only work with 1 parameter
     if len(sys.argv) != 2:
-        print "Usage: spamassassin.py <message body file>"
+        print("Usage: spamassassin.py <message body file>")
         sys.exit(0)
-    initFilter()
+    init_filter()
     courier.xfilter.XFilter = courier.xfilter.DummyXFilter
-    print doFilter(sys.argv[1], [])
+    print(do_filter(sys.argv[1], []))
