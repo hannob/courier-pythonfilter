d2446e888785cd42d653042023021370c64077ff
Gordon Messmer <gordon@dragonsdawn.net>
2019-08-10 11:27:10 -0700

    Release 3.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ implement other local mail policies.
 """
 
 setup(name="courier-pythonfilter",
-      version="3.0",
+      version="3.0.1",
       description="Python filtering architecture for the Courier MTA.",
       long_description=long_description,
       author="Gordon Messmer",
